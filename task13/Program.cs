077b56b29e1818d3ccc45b5f604bebb56549c371
Sergio <sergio977@rambler.ru>
2022-11-22 22:45:39 +0400

    add task13 new

--- task13/Program.cs
+++ task13/Program.cs
@@ -5,4 +5,28 @@
 7 8 - третьей цифры нет
 3 2 6 7 9 - 6
 */
+int GetRandomNumberInRange(int start, int end)
+{
+    int result = 0;
+    result = new Random().Next(start, end + 1);
+    return result;
+}
 
+ 
+
+int randomNumber = GetRandomNumberInRange(1, 100000);
+Console.WriteLine(randomNumber);
+int threetDigit = randomNumber; 
+if (randomNumber > 100)
+{
+    while (threetDigit > 1000)
+    {
+        threetDigit = threetDigit / 10;
+    }
+int numberthree = threetDigit % 10;
+Console.WriteLine($" {numberthree} ");
+}
+else
+{
+    Console.WriteLine($"Третьей цифры нет");
+}
