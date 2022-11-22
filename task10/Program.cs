850ed7a94226b5187a545ab1ddf7e0d5c15320c8
Sergio <sergio977@rambler.ru>
2022-11-22 21:46:33 +0400

    add task10 new1

--- task10/Program.cs
+++ task10/Program.cs
@@ -6,3 +6,20 @@
 9 1 8 - 1
 */
 
+int GetRandomNumberInRange(int start, int end)
+{
+    int result = 0;
+    result = new Random().Next(start, end + 1);
+    return result;
+}
+int GetSecondNumber(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = firstDigit % 10;
+    return secondDigit;
+
+}
+int randomNumber = GetRandomNumberInRange(100, 999);
+Console.WriteLine(randomNumber);
+int newNumber = GetSecondNumber(randomNumber);
+Console.WriteLine($" {newNumber} ");
